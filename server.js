5caeae1f2ab4985726930bbcfa29eb70d169dd68
Dteeech <marshall.isaac@protonmail.com>
2024-04-10 23:41:55 +0200

    the test request is fonctionnal

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 
 // Middleware pour gérer les requêtes d'origine croisée
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "192.168.1.180");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
