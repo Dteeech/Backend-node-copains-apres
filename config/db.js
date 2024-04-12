798420f052d15558925762fad5895d5d9b768fe3
Dteeech <marshall.isaac@protonmail.com>
2024-04-12 11:23:37 +0200

    branche develop

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ export const pool = mysql.createPool({
     connectionLimit: process.env.DB_CONNECTION_LIMIT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     socketPath: process.env.DB_SOCKET_PATH,
