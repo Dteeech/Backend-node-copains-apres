8e1012273e44801a1f278ba9960ccb2cc8dd93de
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-04-13 17:50:47 +0200

    path pour récupérer l'ensemble des annonces, testé et fonctionne

---
+++ controllers/get/getAllAdverts.js
@@ -0,0 +1,14 @@
+import {pool} from "../../config/db.js"
+
+const getAllAdverts = (req, res) => {
+    try {
+        let sql = "SELECT * FROM Advert"
+        pool.query(sql,(err, result) =>{
+            if(err) throw err
+            res.json({result})
+        })
+    } catch (error) {
+        res.status(500).json({ "msg": "Internal server error" });
+    }
+}
+export default getAllAdverts
