6ecda10dbf501b6dfba82b206d20eedf9cddadee
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-04-13 17:49:10 +0200

    path pour récupérer l'ensemble des utilisateurs, testé et fonctionne

---
+++ controllers/get/getAllUsers.js
@@ -0,0 +1,14 @@
+import {pool} from "../../config/db.js"
+
+ const getAllUsers = (req, res) => {
+    try {
+        let sql = "SELECT * FROM User"
+        pool.query(sql,(err, result) =>{
+            if(err) throw err
+            res.json({result})
+        })
+    } catch (error) {
+        res.status(500).json({ "msg": "Internal server error" });
+    }
+}
+export default getAllUsers
