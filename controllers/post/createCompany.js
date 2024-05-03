31a29130c29496c99f4b477038feb7de3b85051a
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-03 13:11:39 +0200

    Route pour créer une entreprise et son user associé - testé et fonctionne

---
+++ controllers/post/createCompany.js
@@ -0,0 +1,34 @@
+import { pool } from "../../config/db.js"
+
+const createCompany = (req, res) => {
+    try {
+        const { siret, name, email, password } = req.body
+    
+        if (!siret || !name || !email || !password) {
+            return res.status(400).json({ error: 'Il manque des paramètres à cette requête' })
+        }
+
+        let sql = "INSERT INTO Company (siret, name) VALUES (?, ?)"
+        pool.query(sql, [siret, name], (err, result) => {
+            if (err) {
+                console.error('Error executing SQL query:', err)
+                return res.status(500).json({ error : 'Internal server error' })
+            }
+
+            let sql1 = "INSERT INTO User (email, password, id_company, is_admin) VALUES (?, ?, ?, ?, ?, 0)"
+            pool.query(sql1, [email, password, siret], (err, result) => {
+            if (err) {
+                console.error('Error executing SQL query:', err)
+                return res.status(500).json({ error : 'Internal server error' })
+            }
+
+            res.json({ result })
+            })
+        })
+    } catch (error) {
+        res.status(500).json({ error : 'Internal server error' })
+    }
+
+}
+
+export default createCompany
--- routes/routes.js
+++ routes/routes.js
@@ -11,28 +11,34 @@ import createAdvert from '../controllers/post/createAdvert.js'
 import createUser from '../controllers/post/createUser.js'
 import createCompany from '../controllers/post/createCompany.js'
 import updateUser from '../controllers/put/updateUser.js'
+import updateAdvert from '../controllers/put/updateAdvert.js'
+import deleteCompany from '../controllers/delete/deleteCompany.js'
 const router = express.Router()
 
 
 //Routes get
-router.get('/users', getAllUsers )
-router.get('/adverts', getAllAdverts )
+router.get('/users', getAllUsers ) // fonctionne
+// requête pour trouver seulement les users
+// requête pour trouver seulement les entreprises ?
+router.get('/adverts', getAllAdverts ) // fonctionne
 router.get('/users/:id/chats', getUserChats) 
 // test : http://localhost:3001/api/user-chats?id_emitter=1&id_receiver=2
-router.get('/users/:id', getUser) 
-router.get('/adverts/:id', getAdvert) 
-router.get('/adverts/:category', getAdvertByCategory) 
+router.get('/users/:id', getUser) //fonctionne
+router.get('/adverts/:id', getAdvert) // fonctionne
+router.get('/adverts/category/:category', getAdvertByCategory) //fonctionne
 
 //Routes post
 router.post('/adverts', createAdvert)
-router.post('/users', createUser)
+router.post('/users', createUser) // fonctionne
 router.post('/users/companies', createCompany)
 
 //Routes update
-router.put('/users/:id', updateUser)
+router.put('/users/:id', updateUser) // fonctionne
+router.put('/adverts/:id', updateAdvert) // fonctionne
 
 //Routes delete
-router.delete('/users/:id', deleteUser)
-router.delete('/adverts/:id', deleteAdvert)
+router.delete('/users/:id', deleteUser) // fonctionne
+router.delete('/adverts/:id', deleteAdvert) // fonctionne
+router.delete('/users/companies/:siret', deleteCompany) // fonctionne
 
 export default router
