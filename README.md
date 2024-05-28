382d6c831b582ae083a1f135b19509df40abcfb8
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-05-28 14:58:00 +0200

    Ajout des infos pour lancer le projet et éléments de conception avec tableau récapitulatif des ressources + création du fichier pour envoyer des messages

--- README.md
+++ README.md
@@ -1,8 +1,75 @@
 # Backend Copains d'Après
 
-## Mettre en ligne
-- push le code sur github
-- se connecter à noc et ouvrir le terminal
-- aller dans le répertoire copains d'après si c'est pas déjà fait
-- faire git pull
-- faire node serveur.js pour redémarer le serveur
+Une API permettant de réserver de faire des opérations CRUD sur les ressources de l'application Copains d'Après.
+
+[Frontend de l'application ici](https://github.com/Dteeech/copains-dapres-V2)
+
+## Table des matières
+
+- [Lancer le projet](##lancer-le-projet)
+- [Conception](##conception)
+  - [Tableau récapitulatif des ressources](###tableau-recapitulatif-des-ressources)
+- [Mise en ligne du code](###mise-en-ligne-du-code)
+
+
+## Lancer le projet
+
+Cloner le projet
+~~~
+git clone https://github.com/Dteeech/Backend-node-copains-apres.git
+~~~
+
+L'installation de Node est nécessaire au fonctionnelemnt de l'API
+Lancer le projet en local (commande à exécuter depuis la racine du projet)
+~~~
+node server.js
+~~~
+
+## Conception 
+
+On désire mettre en ligne un service mise en relation de seniors. Un utilisateur est identifié par un prénom, un nom, un mot de pass et une adresse email.
+
+Les cas d'utilisation utilisateur définis sont les suivants : 
+1. L'utilisateur créer une annonce.
+2. L'utilisateur modifie une annonce.
+3. L'utilisateur supprime une annonce.
+4. L'utilisateur consulte toutes les annonces.
+5. L'utilisateur consulte une annonce.
+6. L'utilisateur consulte une catégorie d'annonces.
+7. L'utilisateur consulte le profil d'un autre utilisateur.
+8. L'utilisateur consulte sa messagerie.
+9. L'utilisateur envoie un message.
+10. L'utilisateur créer un compte.
+11. L'utilisateur met à jour ses données.
+12. L'utilisateur supprime son compte.
+13. Une entreprise se créer un compte.
+14. Une entreprise supprime son compte.
+15. L'utilisateur se connecte.
+
+La connexion utilise un JSONWebToken.
+
+### Tableau récapitulatif des ressources
+
+| Ressource | URL | Méthode HTTP | Paramètres d'URL/Variations | Commentaires |
+|-----------|-----------|-----------|
+| Création d'une annonce | /adverts | POST |  | |
+| Modification d'une annonce | /adverts/{id} | PUT | N'est accessible qu'à l'auteur de la réservation (ou un admin) |
+| Suppression d'une annonce | /adverts/{id} | DELETE | N'est accessible qu'à l'auteur de la réservation (ou un admin) |
+| Liste des annonces | /adverts | GET | | |
+| Consulter une annonce | /adverts/{id} | GET | | |
+| Consulter le profil d'un utilisateur | /users/{id} | GET | | |
+| Accéder à tous les messages envoyés ou reçus par une personne | /users/{id}/chats | GET | | |
+| Envoyer un message |  | POST | | Cette route n'existe pas encore |
+| Créer un compte | /register | POST |  |  |
+| Mettre à jour ses données | /users/{id} | PUT |  |  |
+| Supprimer son compte | /users/{id} | DELETE |  |  |
+| Créer un compte entreprise | /register/companies | POST |  |  |
+| Supprimer son compte entreprise | /users/company/{siret} | DELETE |  |  |
+| Identification | /login | POST |  |  |
+
+## Mise en ligne du code (à l'attention des développeurs)
+- Push le code sur github
+- Se connecter à N0C et ouvrir le terminal
+- Aller dans le répertoire copains d'après si c'est pas déjà fait
+- Exécuter la commande git pull
+- Exécuter la commande node serveur.js pour redémarer le serveur
--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,64 +1,58 @@
 // controllers/authController.js
 
-import jwt from 'jsonwebtoken';
-import { asyncQuery } from '../../config/db.js'; // Importez votre fonction asyncQuery
+import jwt from 'jsonwebtoken'
+import { asyncQuery } from '../../config/db.js'
 
 const register = async (req, res) => {
     try {
-        // Récupérez les données d'inscription de la requête
-        const { first_name, last_name, email, password } = req.body;
+        const { first_name, last_name, email, password } = req.body
 
-        // Vérifiez si tous les champs requis sont présents
         if (!first_name || !last_name || !email || !password) {
-            return res.status(400).json({ error: 'Il manque des paramètres à cette requête' });
+            return res.status(400).json({ error: 'Il manque des paramètres à cette requête' })
         }
 
-        // Vérifiez si l'utilisateur existe déjà dans la base de données
-        const existingUser = await asyncQuery('SELECT * FROM User WHERE email = ?', [email]);
+        // Vérifie si l'utilisateur existe déjà dans la base de données
+        const existingUser = await asyncQuery('SELECT * FROM User WHERE email = ?', [email])
 
         if (existingUser.length > 0) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: 'User already exists' })
         }
 
-        // Créez un nouvel utilisateur dans la base de données
-        const sql = "INSERT INTO User (first_name, last_name, email, password, is_admin) VALUES (?, ?, ?, ?, 0)";
-        await asyncQuery(sql, [first_name, last_name, email, password]);
+        const sql = "INSERT INTO User (first_name, last_name, email, password, is_admin) VALUES (?, ?, ?, ?, 0)"
+        await asyncQuery(sql, [first_name, last_name, email, password])
 
-        // Récupérez le nouvel utilisateur
-        const newUser = await asyncQuery('SELECT * FROM User WHERE email = ?', [email]);
+        // Récupère le nouvel utilisateur
+        const newUser = await asyncQuery('SELECT * FROM User WHERE email = ?', [email])
 
         // Générez un token JWT avec les informations de l'utilisateur
-        const jwtToken = jwt.sign({ userId: newUser[0].id, email, role: 'user' }, `${process.env.JWT_SECRET}`, { expiresIn: '7d' });
+        const jwtToken = jwt.sign({ userId: newUser[0].id, email, role: 'user' }, `${process.env.JWT_SECRET}`, { expiresIn: '7d' })
 
         // Envoyez le token JWT et les données de l'utilisateur au client
-        res.status(201).json({ jwtToken, userData: { id: newUser[0].id, first_name, last_name, email } });
+        res.status(201).json({ jwtToken, userData: { id: newUser[0].id, first_name, last_name, email } })
     } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error('Error registering user:', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
-};
+}
 
 const login = async (req, res) => {
     try {
-        // Récupérez les identifiants de connexion de la requête
-        const { email, password } = req.body;
+        const { email, password } = req.body
 
-        // Récupérez l'utilisateur à partir de la base de données
-        const user = await asyncQuery('SELECT * FROM User WHERE email = ? AND password = ?', [email, password]);
+        const user = await asyncQuery('SELECT * FROM User WHERE email = ? AND password = ?', [email, password])
         if (user.length === 0) {
-            // Identifiants invalides, renvoyez une erreur
-            return res.status(401).json({ message: 'Invalid credentials' });
+            return res.status(401).json({ message: 'Invalid credentials' })
         }
 
-        // Générez un token JWT avec les informations de l'utilisateur
-        const jwtToken = jwt.sign({ userId: user[0].id, email, role: user[0].is_admin ? 'admin' : 'user' }, `${process.env.JWT_SECRET}`, { expiresIn: '7d' });
+        // Génère un token JWT avec les informations de l'utilisateur
+        const jwtToken = jwt.sign({ userId: user[0].id, email, role: user[0].is_admin ? 'admin' : 'user' }, `${process.env.JWT_SECRET}`, { expiresIn: '7d' })
 
         // Envoyez le token JWT et les données de l'utilisateur au client
-        res.json({ jwtToken, userData: user[0] });
+        res.json({ jwtToken, userData: user[0] })
     } catch (error) {
-        console.error('Error logging in:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error('Error logging in:', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
-};
+}
 
-export { login, register };
+export { login, register }
