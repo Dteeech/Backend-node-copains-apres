b9ee7dd2ec61837e59e7e03a404a92b2ecfeb085
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2024-06-08 12:00:13 +0200

    Création de deux requêtes différentes pour catégorie colocation et autres catégories car sinon on ne pouvait pas accéder aux attributs de la table HouseShare

--- controllers/get/getAdvertByCategory.js
+++ controllers/get/getAdvertByCategory.js
@@ -1,19 +1,35 @@
 import { pool } from "../../config/db.js"
 
 const getAdvertByCategory = (req, res) => {
-    try {
-        const { category } = req.params
+    const { category } = req.params
+
+    if (category === "colocation") {
+        try {
     
-        let sql = "SELECT a.id_advert, a.title, a.description, a.location, a.hourly_rate, a.id_category, a.created_at, a.last_updated, a.id_user, a.id_houseshare FROM Advert a JOIN Category c ON a.id_category = c.id_category WHERE c.name = ?"
-        pool.query(sql, [category], (err, result) => {
-            if (err) {
-                console.error('Error executing SQL query:', err)
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.json({ result })
-        })
-    } catch (error) {
-        res.status(500).json({ error : 'Internal server error' })
+            let sql = "SELECT hs.housing_type, hs.location_type, hs.has_garage, hs.is_furnished, hs.surface_area, hs.pets_allowed, hs.wifi_available, hs.low_mobility_friendly, hs.has_garden, hs.rent, a.title, a.description, a.location, a.id_user FROM HouseShare hs JOIN Advert a ON hs.id_houseshare = a.id_houseshare JOIN Category c ON a.id_category = c.id_category WHERE c.name = 'colocation';"
+            pool.query(sql, (err, result) => {
+                if (err) {
+                    console.error('Error executing SQL query:', err)
+                    return res.status(500).json({ error: 'Internal server error' })
+                }
+                res.json({ result })
+            })
+        } catch (error) {
+            res.status(500).json({ error : 'Internal server error' })
+        }
+    } else {
+        try {
+            let sql = "SELECT a.id_advert, a.title, a.description, a.location, a.hourly_rate, a.id_category, a.created_at, a.last_updated, a.id_user, a.id_houseshare FROM Advert a JOIN Category c ON a.id_category = c.id_category WHERE c.name = ?"
+            pool.query(sql, [category], (err, result) => {
+                if (err) {
+                    console.error('Error executing SQL query:', err)
+                    return res.status(500).json({ error: 'Internal server error' })
+                }
+                res.json({ result })
+                })
+        } catch (error) {
+            res.status(500).json({ error : 'Internal server error' })
+        }
     }
 }
 
